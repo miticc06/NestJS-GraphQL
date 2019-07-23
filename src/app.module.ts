59b5e0ffeabdefa95f30da1a2fb67f798643777c
Dang Minh Tien <dangminhtien06@gmail.com>
2019-07-23 09:39:36 +0700

    Add first example (Schema & resolver)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql'
+import { CatsModule } from './cats/cats.module';
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/cats/cats.resolver.ts
@@ -0,0 +1,8 @@
+import { Resolver, Query } from "@nestjs/graphql"
+@Resolver()
+export class CatsResolver {
+    @Query(() => String)
+    async hello() {
+        return 'hello';
+    }
+}
