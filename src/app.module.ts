474b50795ef5c1e14f4970c6290ab00d2db00a9b
Dang Minh Tien <dangminhtien06@gmail.com>
2019-07-23 11:12:27 +0700

    add mutation , query (create Cat, get all cats)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql'
 import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose'
 @Module({
   imports: [
     CatsModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql'
     })
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CatsResolver } from './cats.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatSchema } from './cats.schema';
+import { CatsService } from './cats.service';
 @Module({
-    providers: [CatsResolver],
+    providers: [CatsResolver, CatsService],
+    imports: [
+        MongooseModule.forFeature([{ name: 'Cat', schema: CatSchema }])
+    ]
 })
 export class CatsModule { }
